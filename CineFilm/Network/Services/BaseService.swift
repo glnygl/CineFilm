fa24393e45049fce518e23f1b67c62c610c320a0
Glny Gl <glnygl94@gmail.com>
2024-06-13 16:55:57 +0200

    Delete needless code

--- CineFilm/Network/Services/BaseService.swift
+++ CineFilm/Network/Services/BaseService.swift
@@ -13,16 +13,9 @@ protocol BaseServiceProtocol {
 
 class BaseService: BaseServiceProtocol {
     func performRequest<T:Decodable>(request: BaseRequest, completion:@escaping (Result<T, AFError>) -> Void) {
-        if request.blocks {
-            //loading
-        }
-        
         AF.request(request)
             .responseDecodable (decoder: JSONDecoder()){ (response: DataResponse<T, AFError>) in
                 completion(response.result)
-                if request.blocks {
-                    //loading
-                }
             }
     }
 }
